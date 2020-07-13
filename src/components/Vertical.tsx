a7cc59c5c8b09a50e3531e79cf4b9b10d3c1d07d
m19e <nexus.me7290@gmail.com>
2020-07-14 00:53:33 +0900

    Start development vertical-editor

--- src/components/Vertical.tsx
+++ src/components/Vertical.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Editor, EditorState } from "draft-js";
+import "./Vertical.css";
 
 const Vertical = (): JSX.Element => {
-    return <div></div>;
+    const [editorState, setEditorState] = useState(() =>
+        EditorState.createEmpty()
+    );
+
+    return (
+        <div className="tate">
+            <h1>Draft.js sample</h1>
+            <Editor editorState={editorState} onChange={setEditorState} />
+        </div>
+    );
 };
 
 export default Vertical;
