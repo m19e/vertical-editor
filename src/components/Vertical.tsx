9e5bad449c5ba5f723723206d441a9b414e481cd
m19e <nexus.me7290@gmail.com>
2020-07-20 04:51:34 +0900

    Move caret up, down

--- src/components/Vertical.tsx
+++ src/components/Vertical.tsx
@@ -6,12 +6,76 @@ const Vertical = (): JSX.Element => {
     const [editorState, setEditorState] = useState(() =>
         EditorState.createEmpty()
     );
+    const initialSelection = editorState.getSelection();
+    const [selectionStateInputs, setSelectionStateInputs] = useState({
+        anchorOffset: initialSelection.getAnchorOffset(),
+        focusOffset: initialSelection.getFocusOffset(),
+        anchorKey: initialSelection.getAnchorKey(),
+        focusKey: initialSelection.getFocusKey(),
+    });
 
     const handleArrow = (e: React.KeyboardEvent) => {
+        const selection = editorState.getSelection();
+        setSelectionStateInputs({
+            anchorOffset: selection.getAnchorOffset(),
+            focusOffset: selection.getFocusOffset(),
+            anchorKey: selection.getAnchorKey(),
+            focusKey: selection.getFocusKey(),
+        });
+        // console.log("INPUT:", JSON.stringify(selectionStateInputs, null, 4));
+
         if (e.key.includes("Arrow")) {
-            // e.preventDefault();
-            const currentSelection = editorState.getSelection();
-            console.log(JSON.stringify(currentSelection, null, 4));
+            e.preventDefault();
+
+            const ta = window.getSelection();
+            const start: number = ta?.anchorOffset || 0;
+            if (e.key === "ArrowUp") {
+                if (start - 1 < 0) return null;
+                ta?.setPosition(ta.anchorNode, start - 1);
+            }
+            if (e.key === "ArrowDown") {
+                if (start + 1 > (ta?.anchorNode?.textContent?.length || 0))
+                    return null;
+                ta?.setPosition(ta.anchorNode, start + 1);
+            }
+            console.log(JSON.stringify(window.getSelection()?.anchorOffset));
+
+            // console.log(
+            //     "INPUT:",
+            //     JSON.stringify(selectionStateInputs, null, 4)
+            // );
+            // const {
+            //     anchorOffset,
+            //     focusOffset,
+            //     anchorKey,
+            //     focusKey,
+            // } = selectionStateInputs;
+            // const inc = anchorOffset + 1;
+            // const newSelection = selection.merge({
+            //     anchorOffset: inc,
+            //     focusOffset: inc,
+            //     anchorKey,
+            //     focusKey,
+            // });
+            // console.log("NEW:", JSON.stringify(newSelection, null, 4));
+            // setSelectionStateInputs({
+            //     anchorOffset: inc,
+            //     focusOffset: inc,
+            //     anchorKey,
+            //     focusKey,
+            // });
+            // const newEditor = EditorState.acceptSelection(
+            //     editorState,
+            //     newSelection
+            // );
+            // setEditorState(
+            //     EditorState.push(
+            //         newEditor,
+            //         newEditor.getCurrentContent(),
+            //         "insert-characters"
+            //     )
+            // );
+            // console.log(JSON.stringify(editorState.getSelection(), null, 4));
         }
         return getDefaultKeyBinding(e);
     };
