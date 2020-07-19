f4647993e96a4b2a2ca409a0003e8afaf456a645
m19e <nexus.me7290@gmail.com>
2020-07-19 19:57:10 +0900

    Add onkeydown arrow handler

--- src/components/Vertical.tsx
+++ src/components/Vertical.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Editor, EditorState } from "draft-js";
+import { Editor, EditorState, getDefaultKeyBinding } from "draft-js";
 import "./Vertical.css";
 
 const Vertical = (): JSX.Element => {
@@ -7,10 +7,23 @@ const Vertical = (): JSX.Element => {
         EditorState.createEmpty()
     );
 
+    const handleArrow = (e: React.KeyboardEvent) => {
+        if (e.key.includes("Arrow")) {
+            // e.preventDefault();
+            const currentSelection = editorState.getSelection();
+            console.log(JSON.stringify(currentSelection));
+        }
+        return getDefaultKeyBinding(e);
+    };
+
     return (
         <div className="tate">
             <h1>Draft.js sample</h1>
-            <Editor editorState={editorState} onChange={setEditorState} />
+            <Editor
+                editorState={editorState}
+                onChange={setEditorState}
+                keyBindingFn={handleArrow}
+            />
         </div>
     );
 };
