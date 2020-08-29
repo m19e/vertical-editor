6f32b1f9bf7a4006cc0eae17c75807b9b949161c
m19e <nexus.me7290@gmail.com>
2020-08-29 17:04:42 +0900

    Commit for hosting on github pages

---
+++ build/precache-manifest.6b677e988c9c54b6bb33fc6d5ee86647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06e284742dea32d4f4ddd888fa7ace4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fde477a25e944d01a652",
+    "url": "/static/css/main.60d74d0c.chunk.css"
+  },
+  {
+    "revision": "83aa0f7db73d63fe2607",
+    "url": "/static/js/2.63bfbb4a.chunk.js"
+  },
+  {
+    "revision": "8dea9a823bfb24207fef47cb32cf30ad",
+    "url": "/static/js/2.63bfbb4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde477a25e944d01a652",
+    "url": "/static/js/main.7c81d3b1.chunk.js"
+  },
+  {
+    "revision": "285f2196b415b5356540",
+    "url": "/static/js/runtime-main.8b832764.js"
+  }
+]);
